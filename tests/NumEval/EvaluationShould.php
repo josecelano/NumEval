e1b817a1165e7b3c468f3cad1d0d899abbaa1b7e
Jose Celano <jose.celano@hyve.net>
2020-04-09 18:33:20 +0100

    Add test for arctan

--- tests/NumEval/EvaluationShould.php
+++ tests/NumEval/EvaluationShould.php
@@ -14,4 +14,17 @@ class EvaluationShould extends BaseTestClass {
             '3|154084834456405157341652385/1088199945586205657083492425',
             $evaluation->result_value($result));
     }
+
+    /** @test */
+    public function compute_arctan() {
+        $evaluation = new Evaluation();
+        $result = $evaluation->trigonometry->arctan(1,10);
+
+        // arctan(1) = 0.78539816
+        // result: 0|-156/945 = 0,1650793650793651
+
+        $this->assertEquals(
+            '3|154084834456405157341652385/1088199945586205657083492425',
+            $evaluation->result_value($result));
+    }
 }
